bc4c08503e7167de12ff24cfd97610b69c7f2b0e
discretemathlover <116070234+discretemathlover@users.noreply.github.com>
2025-03-12 12:08:21 +0100

    LoginScreen updated

--- app/src/main/java/com/example/laboralkutxatpv/LogInScreen.kt
+++ app/src/main/java/com/example/laboralkutxatpv/LogInScreen.kt
@@ -1,6 +1,5 @@
 package com.example.laboralkutxatpv
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -10,7 +9,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.graphics.Color
-import android.graphics.drawable.Drawable
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 
